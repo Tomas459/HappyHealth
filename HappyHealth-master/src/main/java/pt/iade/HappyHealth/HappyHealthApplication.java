da11aa235d55205705109e200824cc8578342721
joaopedro.fbz <20200255@iade.pt>
2021-06-12 20:19:24 +0100

    initial commit
    this is the initial commit

---
+++ HappyHealth-master/src/main/java/pt/iade/HappyHealth/HappyHealthApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.HappyHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHealthApplication.class, args);
+	}
+
+}
---
+++ HappyHealth-master/src/test/java/pt/iade/HappyHealth/HappyHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.HappyHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
